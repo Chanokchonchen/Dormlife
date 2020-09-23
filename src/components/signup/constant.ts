7d8f1132e8395efeae40c361d5a1af2e9bd660b0
Chaiviwat Chuenchart <chaiviwat.ch@ku.th>
2020-09-23 15:02:27 +0700

    fff

--- src/components/signup/constant.ts
+++
@@ -1,6 +0,0 @@
-import {propRadioInput } from './typeForm'
-export const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
-export const  gender : propRadioInput[] = [
-    {name:"sex",label:"Male",value:"male"},
-    {name:"sex",label:"Female",value:"female"}
-]
